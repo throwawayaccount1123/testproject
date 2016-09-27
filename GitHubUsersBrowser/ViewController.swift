fea0c376d38f19da998b474a2c15e4d20b927d0a
Sergey Lem <lemsergey@gmail.com>
2016-09-27 16:56:25 +0300

    Initial commit

---
+++ GitHubUsersBrowser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubUsersBrowser
+//
+//  Created by Sergey Lem on 9/27/16.
+//  Copyright © 2016 Sergey Lem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
